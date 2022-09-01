dec2d11a39a00d0ec60f427ff344fdd3a9633587
TarkLiao-egg <tark_liao@hengyuan.com.tw>
2022-09-01 11:32:42 +0800

    Update GRDB

--- Utility/GRDB/GRDBController.swift
+++ Utility/GRDB/GRDBController.swift
@@ -122,20 +122,68 @@ class Player: DBInit, FetchableRecord, PersistableRecord, Codable, Comparable {
     var timestamp: Int
     var times: Int
     var exerciseTimeStr: String
+    var imageData: Data?
+    
     static func < (lhs: Player, rhs: Player) -> Bool {
         return lhs.timestamp > rhs.timestamp
     }
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case timestamp = "timestamp"
+        case times = "times"
+        case exerciseTimeStr = "exerciseTimeStr"
+        case imageData = "imageData"
+    }
     
-    init(date: Date, times: Int, exStr: String) {
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.timestamp = try container.decode(Int.self, forKey: .timestamp)
+        self.times = try container.decode(Int.self, forKey: .times)
+        self.exerciseTimeStr = try container.decode(String.self, forKey: .exerciseTimeStr)
+        self.imageData = try container.decode(Data.self, forKey: .imageData)
+        super.init()
+        self.id = try? container.decode(Int.self, forKey: .id)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(timestamp, forKey: .timestamp)
+        try container.encode(times, forKey: .times)
+        try container.encode(exerciseTimeStr, forKey: .exerciseTimeStr)
+        try container.encode(imageData, forKey: .imageData)
+    }
+    
+    init(date: Date, times: Int, exStr: String, image: UIImage? = nil) {
         self.timestamp = date.getTimeStamp()
         self.times = times
         self.exerciseTimeStr = exStr
         super.init()
+        if let image = image {
+            saveImage(image)
+        }
     }
     
     required override init() {
         self.timestamp = Date().getTimeStamp()
         self.times = 0
         self.exerciseTimeStr = ""
+    }
+    
+    func saveImage(_ image: UIImage?) {
+        if let nsdata = image?.pngData() {
+            self.imageData = nsdata
+        } else if let nsdata = image?.jpegData(compressionQuality: 0.5) {
+            self.imageData = nsdata
+        } else {
+            self.imageData = nil
+        }
+    }
+    
+    func loadImage() -> UIImage? {
+        if let data = imageData {
+            return UIImage(data: data)
+        }
+        return nil
     }
 }
